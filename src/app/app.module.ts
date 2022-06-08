30eb8ac94b6fe280058b90118280d59ffa485aa1
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-06-08 16:15:49 -0300

    Curso Angular #14: Emitindo Eventos com Output properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputPropertyComponent
   ],
   imports: [
     BrowserModule
