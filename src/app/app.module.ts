6ba251695526dba920e468f0bae3b6f8305fae6f
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-06-10 14:44:04 -0300

    Curso Angular #26: Diretivas: ngSwitch, ngSwitchCase e ngSwitchDefault

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OutputPropertyComponent,
-    DiretivaNgifComponent
+    DiretivaNgifComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
     BrowserModule
